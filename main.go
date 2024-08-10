fc184eea4064d305c5bf7f06f74cb1df990730c4
Takahito Urai <uraitakahito@gmail.com>
2024-08-10 16:00:13 +0900

    Add `run`

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -17,4 +21,31 @@ func main() {
 		fmt.Printf("Server failed: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		// http.ErrServerClosedは
+		// http.Server.Shutdown()が正常に終了したことを示すので異常ではない
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// チャネルからの通知(終了通知)を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// Goメソッドで起動した別ゴルーチンの終了をまつ
+	return eg.Wait()
 }
